466e538ba11c65c7c3b28dcdfd12f580e1d67932
makc-anisimov <makc.anisimov@gmail.com>
2024-10-29 16:11:27 +0300

    configure app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "../../store";
 import {
   AddEmployeePage,
   EditEmployeePage,
   EmployeesPage,
   NotFoundPage,
-} from "./pages";
+} from "../../pages";
 
 const App: React.FC = () => {
   return (
